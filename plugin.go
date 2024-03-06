78513862f2a6b7866713bee5c288be116a62c08f
David Ferlay <dferlay@skilld.cloud>
2024-03-06 19:39:20 +0100

    Introducing goroutine to parallelize some commands

--- plugin.go
+++ plugin.go
@@ -7,8 +7,7 @@ import (
 	"github.com/launchrctl/launchr/pkg/cli"
 	"os"
 	"os/exec"
-
-	"io"
+	"sync"
 
 	"github.com/launchrctl/keyring"
 	"github.com/launchrctl/launchr"
@@ -19,7 +18,7 @@ func init() {
 	launchr.RegisterPlugin(&Plugin{})
 }
 
-// Plugin is launchr plugin providing bump action.
+// Plugin is launchr plugin providing meta action.
 type Plugin struct {
 	k keyring.Keyring
 }
@@ -27,7 +26,7 @@ type Plugin struct {
 // PluginInfo implements launchr.Plugin interface.
 func (p *Plugin) PluginInfo() launchr.PluginInfo {
 	return launchr.PluginInfo{
-		Weight: 10,
+		Weight: 20,
 	}
 }
 
@@ -62,45 +61,112 @@ func (p *Plugin) CobraAddCommands(rootCmd *cobra.Command) error {
 
 func meta(environment, resources string, k keyring.Keyring) error {
 	cli.Println("")
-	cmd := exec.Command("plasmactl", "bump")
-	var stdout, stderr bytes.Buffer
-	cmd.Stdout = io.MultiWriter(os.Stdout, &stdout)
-	cmd.Stderr = io.MultiWriter(os.Stderr, &stderr)
-	cmd.Stdin = os.Stdin
-	_ = cmd.Run()
+	//cmd := exec.Command("plasmactl", "bump")
+	var err error
+	//cmd.Stdout = os.Stdout
+	//cmd.Stderr = os.Stderr
+	//cmd.Stdin = os.Stdin
+	//_ = cmd.Run()
+
+	//fmt.Println()
+	//cmd = exec.Command("plasmactl", "compose", "--skip-not-versioned", "--conflicts-verbosity")
+	//cmd.Stdout = os.Stdout
+	//cmd.Stderr = os.Stderr
+	//cmd.Stdin = os.Stdin
+	//err := cmd.Run()
+	//if err != nil {
+	//	if exitErr, ok := err.(*exec.ExitError); ok {
+	//		os.Exit(exitErr.ExitCode())
+	//	} else {
+	//		fmt.Println("Error:", err)
+	//		os.Exit(1)
+	//	}
+	//}
+
+	//fmt.Println()
+	//cmd = exec.Command("plasmactl", "platform:sync", "dev")
+	//cmd.Stdout = os.Stdout
+	//cmd.Stderr = os.Stderr
+	//cmd.Stdin = os.Stdin
+	//err = cmd.Run()
+	//if err != nil {
+	//	if exitErr, ok := err.(*exec.ExitError); ok {
+	//		os.Exit(exitErr.ExitCode())
+	//	} else {
+	//		fmt.Println("Error:", err)
+	//		os.Exit(1)
+	//	}
+	//}
 
 	fmt.Println()
-	cmd = exec.Command("plasmactl", "compose", "--skip-not-versioned", "--conflicts-verbosity")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
-	err := cmd.Run()
-	if err != nil {
-		if exitErr, ok := err.(*exec.ExitError); ok {
-			os.Exit(exitErr.ExitCode())
-		} else {
-			fmt.Println("Error:", err)
-			os.Exit(1)
+	var packageStdOut bytes.Buffer
+	var packageStdErr bytes.Buffer
+	var packageErr error
+	var publishStdOut bytes.Buffer
+	var publishStdErr bytes.Buffer
+	var publishErr error
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		fmt.Println("100")
+		defer wg.Done()
+		packageCmd := exec.Command("plasmactl", "package")
+		packageCmd.Stdout = &packageStdOut
+		packageCmd.Stderr = &packageStdErr
+		packageCmd.Stdin = os.Stdin
+		packageErr = packageCmd.Run()
+		if packageErr != nil {
+			return
 		}
-	}
+		publishCmd := exec.Command("plasmactl", "publish")
+		publishCmd.Stdout = &publishStdOut
+		publishCmd.Stderr = &publishStdErr
+		publishCmd.Stdin = os.Stdin
+		publishErr = publishCmd.Run()
+		if publishErr != nil {
+			return
+		}
+	}(wg)
 
-	fmt.Println()
-	cmd = exec.Command("plasmactl", "platform:sync", "dev")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
-	err = cmd.Run()
-	if err != nil {
-		if exitErr, ok := err.(*exec.ExitError); ok {
-			os.Exit(exitErr.ExitCode())
-		} else {
-			fmt.Println("Error:", err)
-			os.Exit(1)
-		}
+	//wg.Add(1)
+	//go func(wg *sync.WaitGroup) {
+	//	defer wg.Done()
+	//	cmd = exec.Command("plasmactl", "platform:deploy", "dev", "interaction.applications.repositories")
+	//	cmd.Stdout = os.Stdout
+	//	cmd.Stderr = os.Stderr
+	//	cmd.Stdin = os.Stdin
+	//	err = cmd.Run()
+	//	if err != nil {
+	//		if exitErr, ok := err.(*exec.ExitError); ok {
+	//			os.Exit(exitErr.ExitCode())
+	//		} else {
+	//			fmt.Println("Error:", err)
+	//			os.Exit(1)
+	//		}
+	//	}
+	//}(wg)
+	wg.Wait()
+	fmt.Println(packageStdOut.String())
+	fmt.Println(packageStdErr.String())
+	fmt.Println(publishStdOut.String())
+	fmt.Println(publishStdErr.String())
+	if exitErr, ok := err.(*exec.ExitError); ok {
+		os.Exit(exitErr.ExitCode())
+	} else {
+		fmt.Println("Error:", err)
+		os.Exit(1)
 	}
 
 	return nil
 }
+
+//TODO:
+// - wrap stdin to buffer and display it after everything
+// - make sure exit code is forwaded after wait group
+// - change cmd. by other names
+// - check if package does create artifact in background
+// - check if with publish keyring, artifact is uploaded in background
+// - implement same options as commands used
 
 //return errors.New("error opening artifact file")
 //log.Info("LOG INFO")
