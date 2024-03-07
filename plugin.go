dadb0c001fb9f8314831ac56395030be03723b51
David Ferlay <dferlay@skilld.cloud>
2024-03-07 12:03:20 +0100

    Refactoring code after goroutine implementation

--- plugin.go
+++ plugin.go
@@ -26,7 +26,7 @@ type Plugin struct {
 // PluginInfo implements launchr.Plugin interface.
 func (p *Plugin) PluginInfo() launchr.PluginInfo {
 	return launchr.PluginInfo{
-		Weight: 20,
+		Weight: 1337,
 	}
 }
 
@@ -60,110 +60,129 @@ func (p *Plugin) CobraAddCommands(rootCmd *cobra.Command) error {
 }
 
 func meta(environment, resources string, k keyring.Keyring) error {
-	cli.Println("")
-	//cmd := exec.Command("plasmactl", "bump")
-	var err error
-	//cmd.Stdout = os.Stdout
-	//cmd.Stderr = os.Stderr
-	//cmd.Stdin = os.Stdin
-	//_ = cmd.Run()
-
-	//fmt.Println()
-	//cmd = exec.Command("plasmactl", "compose", "--skip-not-versioned", "--conflicts-verbosity")
-	//cmd.Stdout = os.Stdout
-	//cmd.Stderr = os.Stderr
-	//cmd.Stdin = os.Stdin
-	//err := cmd.Run()
-	//if err != nil {
-	//	if exitErr, ok := err.(*exec.ExitError); ok {
-	//		os.Exit(exitErr.ExitCode())
-	//	} else {
-	//		fmt.Println("Error:", err)
-	//		os.Exit(1)
-	//	}
-	//}
-
-	//fmt.Println()
-	//cmd = exec.Command("plasmactl", "platform:sync", "dev")
-	//cmd.Stdout = os.Stdout
-	//cmd.Stderr = os.Stderr
-	//cmd.Stdin = os.Stdin
-	//err = cmd.Run()
-	//if err != nil {
-	//	if exitErr, ok := err.(*exec.ExitError); ok {
-	//		os.Exit(exitErr.ExitCode())
-	//	} else {
-	//		fmt.Println("Error:", err)
-	//		os.Exit(1)
-	//	}
-	//}
+	bumpCmd := exec.Command("plasmactl", "bump")
+	bumpCmd.Stdout = os.Stdout
+	bumpCmd.Stderr = os.Stderr
+	bumpCmd.Stdin = os.Stdin
+	_ = bumpCmd.Run()
 
 	fmt.Println()
+	composeCmd := exec.Command("plasmactl", "compose", "--skip-not-versioned", "--conflicts-verbosity", "--keyring-passphrase", "X")
+	composeCmd.Stdout = os.Stdout
+	composeCmd.Stderr = os.Stderr
+	composeCmd.Stdin = os.Stdin
+	composeErr := composeCmd.Run()
+	if composeErr != nil {
+		if exitErr, ok := composeErr.(*exec.ExitError); ok {
+			os.Exit(exitErr.ExitCode())
+		} else {
+			fmt.Println("Error:", composeErr)
+			os.Exit(1)
+		}
+	}
+
+	fmt.Println()
+	syncCmd := exec.Command("plasmactl", "platform:sync", "dev")
+	//syncCmd = exec.Command("plasmactl", "bump", "--sync", "dev", "--keyring-passphrase", "X")
+	syncCmd.Stdout = os.Stdout
+	syncCmd.Stderr = os.Stderr
+	syncCmd.Stdin = os.Stdin
+	syncErr := syncCmd.Run()
+	if syncErr != nil {
+		if exitErr, ok := syncErr.(*exec.ExitError); ok {
+			os.Exit(exitErr.ExitCode())
+		} else {
+			fmt.Println("Error:", syncErr)
+			os.Exit(1)
+		}
+	}
+
 	var packageStdOut bytes.Buffer
 	var packageStdErr bytes.Buffer
 	var packageErr error
+
 	var publishStdOut bytes.Buffer
 	var publishStdErr bytes.Buffer
 	var publishErr error
+
+	fmt.Println()
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
 	go func(wg *sync.WaitGroup) {
-		fmt.Println("100")
+		cli.Println("--Starting waitgroup 1")
 		defer wg.Done()
+
 		packageCmd := exec.Command("plasmactl", "package")
 		packageCmd.Stdout = &packageStdOut
 		packageCmd.Stderr = &packageStdErr
-		packageCmd.Stdin = os.Stdin
+		//publishCmd.Stdin = os.Stdin // Any interaction will prevent waitgroup to finish and thus stuck before print of stdout
 		packageErr = packageCmd.Run()
 		if packageErr != nil {
 			return
 		}
-		publishCmd := exec.Command("plasmactl", "publish")
+
+		//publishCmd := exec.Command("plasmactl", "publish")
+		publishCmd := exec.Command("plasmactl", "publish", "--keyring-passphrase", "X")
 		publishCmd.Stdout = &publishStdOut
 		publishCmd.Stderr = &publishStdErr
-		publishCmd.Stdin = os.Stdin
+		//publishCmd.Stdin = os.Stdin // Any interaction will prevent waitgroup to finish and thus stuck before print of stdout
 		publishErr = publishCmd.Run()
 		if publishErr != nil {
 			return
 		}
+		cli.Println("--Exiting waitgroup 1")
 	}(wg)
 
-	//wg.Add(1)
-	//go func(wg *sync.WaitGroup) {
-	//	defer wg.Done()
-	//	cmd = exec.Command("plasmactl", "platform:deploy", "dev", "interaction.applications.repositories")
-	//	cmd.Stdout = os.Stdout
-	//	cmd.Stderr = os.Stderr
-	//	cmd.Stdin = os.Stdin
-	//	err = cmd.Run()
-	//	if err != nil {
-	//		if exitErr, ok := err.(*exec.ExitError); ok {
-	//			os.Exit(exitErr.ExitCode())
-	//		} else {
-	//			fmt.Println("Error:", err)
-	//			os.Exit(1)
-	//		}
-	//	}
-	//}(wg)
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		cli.Println("--Starting waitgroup 2")
+		defer wg.Done()
+		//deployCmd := exec.Command("plasmactl", "platform:deploy", "dev", "interaction.applications.repositories")
+		deployCmd := exec.Command("ls", "-lah")
+		deployCmd.Stdout = os.Stdout
+		deployCmd.Stderr = os.Stderr
+		deployCmd.Stdin = os.Stdin
+		deployErr := deployCmd.Run()
+		if deployErr != nil {
+			if exitErr, ok := deployErr.(*exec.ExitError); ok {
+				os.Exit(exitErr.ExitCode())
+			} else {
+				fmt.Println("Error:", deployErr)
+				os.Exit(1)
+			}
+		}
+		cli.Println("--Ending waitgroup 2")
+	}(wg)
 	wg.Wait()
+
+	fmt.Println()
 	fmt.Println(packageStdOut.String())
 	fmt.Println(packageStdErr.String())
+	if packageErr != nil {
+		if exitErr, ok := packageErr.(*exec.ExitError); ok {
+			os.Exit(exitErr.ExitCode())
+		} else {
+			fmt.Println("Error:", packageErr)
+			os.Exit(1)
+		}
+	}
+
+	fmt.Println()
 	fmt.Println(publishStdOut.String())
 	fmt.Println(publishStdErr.String())
-	if exitErr, ok := err.(*exec.ExitError); ok {
-		os.Exit(exitErr.ExitCode())
-	} else {
-		fmt.Println("Error:", err)
-		os.Exit(1)
+	if publishErr != nil {
+		if exitErr, ok := publishErr.(*exec.ExitError); ok {
+			os.Exit(exitErr.ExitCode())
+		} else {
+			fmt.Println("Error:", publishErr)
+			os.Exit(1)
+		}
 	}
 
 	return nil
 }
 
 //TODO:
-// - wrap stdin to buffer and display it after everything
-// - make sure exit code is forwaded after wait group
-// - change cmd. by other names
 // - check if package does create artifact in background
 // - check if with publish keyring, artifact is uploaded in background
 // - implement same options as commands used
